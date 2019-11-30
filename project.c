95b5b7a488eaffc19e700c0c3be1dc48c2b4590e
II7-code <insfamily01@naver.com>
2019-12-01 04:33:08 +0900

    commit2

--- project.c
+++ project.c
@@ -1,6 +1,17 @@
-//commit1 project.c
+//commit2 project.c
 #include<stdio.h>
 
+void getGugudan(int danNum){
+    int i;
+    for(i=1;i<=9;i++){
+        printf("%d x %d = %d",danNum,i,danNum*i);
+    }
+}
+
 int main(){
-    printf("This is just begin");
+    int danNum;
+    printf("I want get gugudan \n");
+    printf("what you want dan? ");
+    scanf("%d",&danNum);
+    getGugudan(danNum);
 }
